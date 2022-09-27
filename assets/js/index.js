7d7a5ea4bf8c12437b229d6ff21ef752d2f72ae1
Lautaro Narizzano <lautaronarizzano@gmail.com>
2022-09-27 18:54:18 -0300

    termine segunda entrega JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,87 +24,97 @@ let usuarios = [
 ]
 
 
-let usuarioLogueado
-
-
 verificacion = 3
 
 function iniciarSesion() {
-    while(verificacion > 0) {
-        const login = new inicioSesion(prompt('Ingrese su nombre de usuario:'), prompt('Ingrese su contraseña:'))
+    while (verificacion > 0) {
+        let usuarioLogueado = localStorage.getItem('usuario')
 
-        usuarios.forEach((usuario) => {
-            if (usuario.usuario === login.usuario && usuario.password === login.password) {
-            usuarioLogueado = login
+        if (usuarioLogueado) { 
             verificacion -= 3
-            alert(`su cunta ha sido iniciada con exito, Bienvenido!`)
+            let bienvenido = document.querySelector("#saludo")
+            usuarioLogueado = JSON.parse(usuarioLogueado)
+            bienvenido.innerHTML = `Bienvenido ${usuarioLogueado.usuario} !!!`
+
+        } else { 
+            const login = new inicioSesion(prompt('Ingrese su nombre de usuario:'), prompt('Ingrese su contraseña:'))
+
+            usuarios.forEach((usuario) => {
+                if (usuario.usuario === login.usuario && usuario.password === login.password) {
+                    verificacion -= 3
+
+                    const usuarioParseado = JSON.stringify(login)
+                    localStorage.setItem('usuario', usuarioParseado)
+
+                    alert(`su cuenta ha sido iniciada con exito, Bienvenido!`)
+                    let bienvenido = document.querySelector("#saludo")
+                    bienvenido.innerHTML = `Bienvenido ${login.usuario} !!!`
+
+                }
+            })
+
+            if (verificacion > 0) alert('Lo siento, el usuario o la contraseña son incorrectas, vuelve a intentarlo')
         }
     }
-    )
-    if (!usuarioLogueado) alert('Lo siento, el usuario o la contraseña son incorrectas, vuelve a intentarlo')
-    
-}
 }
 
 iniciarSesion()
-let nombre = prompt("Ingrese su nombre")
 
 
-    //dom
-    let bienvenido = document.querySelector(".section-1__h2")
-    bienvenido.innerHTML = `Bienvenido ${nombre}!!!`
 
-    function transferencia() {
 
-        let inputNombre = document.querySelector("#nombre")
-    
-        let inputCvu = document.querySelector("#cvu")
-    
-        let inputDineroEnviar = document.querySelector("#dineroEnviar")
-    
-        let inputMensaje = document.querySelector("#mensaje")
-    
-        let enviarTransaccion = document.querySelector("#enviarTransaccion")
-    
-        enviarTransaccion.addEventListener('click', () => {
-    
-            let receptor = new transaccion(
-    
-                inputNombre.value,
-    
-                inputCvu.value,
-    
-                inputDineroEnviar.value,
-    
-                inputMensaje.value)
-    
-            alert(`Gracais por confiar en nosotros! se ha realizado una transferencia a ${receptor.nombre} de un monto de ${receptor.monto}`)
-    
-        })
-    
-    }
-    
-    transferencia()
-function dolares () {
+function transferencia() {
+
+    let inputNombre = document.querySelector("#nombre")
+
+    let inputCvu = document.querySelector("#cvu")
+
+    let inputDineroEnviar = document.querySelector("#dineroEnviar")
+
+    let inputMensaje = document.querySelector("#mensaje")
+
+    let enviarTransaccion = document.querySelector("#enviarTransaccion")
+
+    enviarTransaccion.addEventListener('click', () => {
+
+        let receptor = new transaccion(
+
+            inputNombre.value,
+
+            inputCvu.value,
+
+            inputDineroEnviar.value,
+
+            inputMensaje.value)
+
+        alert(`Gracais por confiar en nosotros! se ha realizado una transferencia a ${receptor.nombre} de un monto de $${receptor.monto}`)
+
+    })
+
+}
+
+transferencia()
+
+function dolares() {
 
     const COTIZACION_DOLAR = 240
-    
+
     const inputDolares = document.querySelector('#dolar')
     const inputPesos = document.querySelector('#peso')
     const inputImpuesto = document.querySelector('#impuestos')
-    
+
     function convertirAPesos() {
         return parseInt(inputDolares.value) * 240
     }
-    
+
     function convertirADolares() {
         return parseInt(inputPesos.value) / 240
     }
-    
+
     function convertirAImpuestos() {
-        return parseInt(inputPesos.value) + (parseInt(inputPesos.value)*(75/100))
+        return parseInt(inputPesos.value) + (parseInt(inputPesos.value) * (75 / 100))
     }
-    
+
     inputDolares.addEventListener('keyup', () => {
 
         inputPesos.value = convertirAPesos()
@@ -129,17 +139,17 @@ function dolares () {
 
 dolares()
 
-function plazoFijo () {
+function plazoFijo() {
     const inputDinero = document.querySelector("#dinero")
     const inputMeses = document.querySelector("#meses")
     const inputTotal = document.querySelector("#total")
 
     const finalizarMeses = () => {
-        return parseInt(inputDinero.value * (65.9/100))
+        return parseInt(inputDinero.value * (65.9 / 100))
     }
 
     const dineroTotal = () => {
-        return parseInt(inputDinero.value) + (parseInt(inputDinero.value) * (65.9/100))
+        return parseInt(inputDinero.value) + (parseInt(inputDinero.value) * (65.9 / 100))
     }
 
 
@@ -159,17 +169,17 @@ function plazoFijo () {
 
 plazoFijo()
 
-function prestamo () {
+function prestamo() {
     let inputPedir = document.querySelector("#dineroPedir")
     let inputInteres = document.querySelector("#dineroInteres")
     let inputDineroFinal = document.querySelector("#dineroFinal")
     const dineroInteres = () => {
-        return parseInt(inputPedir.value) * (86/100)
+        return parseInt(inputPedir.value) * (86 / 100)
     }
     const dineroFinal = () => {
-    
+
         return parseInt(inputPedir.value) + (parseInt(inputPedir.value) * (86 / 100))
-    
+
     }
     inputPedir.addEventListener('keyup', () => {
         inputInteres.value = dineroInteres()
@@ -182,6 +192,6 @@ function prestamo () {
     btnPrestamo.addEventListener('click', () => {
         alert(`Usted ha pedido un prestamo de $${inputPedir.value} con un interes de $${inputInteres.value} y un plazo final a pagar de $${inputDineroFinal.value} en 12 meses`)
     })
-    }
+}
 
 prestamo()
